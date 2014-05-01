c2ce057879d5aeed9dd08a725c9e04a09c8a9d51
Jonathan Squire <jsquire@Jonathan-Squires-MacBook-Pro.local>
2014-05-01 12:10:29 -0400

    Initial Commit

---
+++ MRIDSS/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  MRIDSS
+//
+//  Created by Jonathan Squire on 1/05/14.
+//  Copyright (c) 2014 Jonathan Squire. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
